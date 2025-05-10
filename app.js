991c52f6916ffccf4d42ccf7d870d305667c4ee9
Derek Harnett <DHarnett.dev@proton.me>
2025-05-10 07:56:40 -0700

    display book information for sample data

--- app.js
+++ app.js
@@ -6,8 +6,29 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 const port = 3000;
 
+const appData = {
+    books: [
+        {
+            title: "One Market Under God",
+            authors: ["Thomas Frank"],
+            coverUrl: "https://covers.openlibrary.org/b/olid/OL7440480M-M.jpg",
+            dateFinished: new Date(2025, 5, 10),
+            description: "A book about free market fundamentalism",
+            rating: 4,
+        }, 
+        {
+            title: "The Corporation",
+            authors: ["Joel Bakan"],
+            coverUrl: "https://covers.openlibrary.org/b/olid/OL7928295M-M.jpg",
+            dateFinished: new Date(2025, 4, 30),
+            description: "Compares corporations to psychopaths",
+            rating: 5,
+        }
+    ]
+};
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", appData);
 });
 
 app.post("/create", (req, res) => {
