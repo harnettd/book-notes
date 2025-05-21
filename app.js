8088b801d67f5aafa29d70e9b37f4e57441c44c4
Derek Harnett <DHarnett.dev@proton.me>
2025-05-20 21:19:49 -0700

    read book data from database

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { Client } from "pg";
 import { user, password } from "./secrets/secrets.js";
-import { addNewBook } from "./modules/database-api.js";
+import { addNewBook, getBooks } from "./modules/database-api.js";
 
 const client = new Client({
   user,
@@ -15,38 +15,13 @@ await client.connect();
 const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-const port = 3000;
+const port = 5000;
 
-// const appData = {
-//   books: [
-//     {
-//       editionKey: "OL2668554W",
-//       title: "One Market Under God",
-//       authors: [{ key: "OL389289A", name: "Thomas Frank" }],
-//       firstPublishYear: 2000,
-//       coverUrl: "https://covers.openlibrary.org/b/olid/OL7440480M-M.jpg",
-//       dateFinished: new Date(2025, 5, 10),
-//       comments: "A book about free market fundamentalism",
-//       rating: 4,
-//     },
-//     {
-//       editionKey: "OL2887824W",
-//       title: "The Corporation",
-//       authors: [{ key: "OL437980A", name: "Joel Bakan" }],
-//       firstPublishYear: 2004,
-//       coverUrl: "https://covers.openlibrary.org/b/olid/OL7928295M-M.jpg",
-//       dateFinished: new Date(2025, 4, 30),
-//       comments: "Compares corporations to psychopaths",
-//       rating: 5,
-//     },
-//   ],
-// };
-
-// await addNewBook(client, appData.books[0]);
 // await addNewBook(client, appData.books[1]);
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", appData);
+app.get("/", async (req, res) => {
+  const books = await getBooks(client);
+  res.render("index.ejs", { books });
 });
 
 app.post("/create", (req, res) => {
--- modules/database-api.js
+++ modules/database-api.js
@@ -20,7 +20,7 @@ const addNewBook = async (
   await client.query(text, values);
 
   authors.forEach(async ({ key, name }) => {
-    // TODO: CHeck if the author is already in the database.
+    // TODO: Check if the author is already in the database.
     text = `
       INSERT INTO authors VALUES
         ($1, $2);
@@ -44,4 +44,62 @@ const addNewBook = async (
   await client.query(text, values);
 };
 
-export { addNewBook };
+const getAuthorNames = (client, editionKey) => {
+  const text = `
+          SELECT
+            author_name
+          FROM books
+          JOIN authors_books using(edition_key)
+          JOIN authors using(author_key)
+          WHERE edition_key = $1;
+        `;
+  const values = [editionKey];
+  return new Promise((fulfill, reject) => {
+    client.query(text, values)
+      .then((results) => {
+        const authors = results.rows;
+        const authorNames = authors.map(({ author_name }) => author_name);
+        fulfill(authorNames);
+      })
+      .catch((err) => reject(err));
+  });
+};
+  
+const addAuthorsToBooks = (books, authorNamesArrays) => {
+  // The arrays books and authorNamesArrays ought to be of the same length.
+  return books.map((book, idx) => {
+    return {
+      ...book,
+      authorNames: authorNamesArrays[idx],
+    };
+  });
+};
+
+const getBooks = async (client) => {
+  let text = `
+        SELECT 
+	        edition_key,
+	        title,
+	        first_publication_year,
+	        cover_art_url,
+	        date_finished,
+	        comments,
+	        rating
+        FROM books
+        JOIN personal_notes USING(edition_key);
+      `;
+  const results = await client.query(text);
+  const books = results.rows;
+  const getAllAuthorNames = books.map(({ edition_key }) => {
+    // console.log(edition_key);
+    return getAuthorNames(client, edition_key);
+  });
+  // getAllAuthorNames.forEach((p) => {
+  //   console.log(p);
+  //   console.log(p.result);
+  // });
+  return Promise.all(getAllAuthorNames)
+    .then((authorNamesArrays) => addAuthorsToBooks(books, authorNamesArrays));
+  };
+
+export { addNewBook, getBooks };
